18f910e4269d3b66869971d7817778abf16081f4
Ishan <ikushum@gmail.com>
2017-04-10 17:24:42 +0545

    seperated bankservices component

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {BankservicesComponent} from './bankservices.component'
 
 @Component({
     selector: 'myapp',
-    templateUrl: 'app/ts/app.component.html'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [BankservicesComponent]
 })
 
 export class AppComponent {}
---
+++ app/ts/bankservices.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'bankservices',
+    templateUrl: 'app/ts/bankservices.component.html'
+})
+
+export class BankservicesComponent {}
