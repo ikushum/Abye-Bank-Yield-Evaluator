d3791fd31ecbdcd4b48d018c45ad063735ed6db0
Ishan <ikushum@gmail.com>
2017-07-18 13:19:23 +0545

    bug fix

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -2,13 +2,12 @@ import {Component} from 'angular2/core';
 import {BankservicesComponent} from './bankservices.component'
 import {LoanComponent} from './loan.component'
 import {DepositComponent} from './deposit.component'
-import {BanksComponent} from './banks.component'
 
 
 @Component({
     selector: 'myapp',
     templateUrl: 'app/ts/html_components/app.component.html',
-    directives: [BankservicesComponent,LoanComponent,DepositComponent,BanksComponent]
+    directives: [BankservicesComponent,LoanComponent,DepositComponent]
 })
 
 export class AppComponent {}
