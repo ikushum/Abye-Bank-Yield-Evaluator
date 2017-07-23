4c6b1ecf4c7c8c6eec2443df9a5b0bae0a378807
Ishan <ikushum@gmail.com>
2017-07-23 18:32:50 +0545

    displayed selected banks

--- app/ts/loan_banks.component.ts
+++ app/ts/loan_banks.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from 'angular2/core';
 import {Obj2Arr} from './obj2arr.pipe';
+import {NgModel} from 'angular2/forms';
 
 
 @Component({
@@ -9,5 +10,29 @@ import {Obj2Arr} from './obj2arr.pipe';
 })
 
 export class BanksComponent {
-	@Input() banks: string;
+    @Input() banks: string;
+    selectedEmployees = [];
+
+    constructor() {}
+  
+    
+    change(obj){
+        let updateItem = this.selectedEmployees.find(this.findIndexToUpdate, obj));
+        let index = this.selectedEmployees.indexOf(updateItem);
+        console.log(index);
+        if(index > -1){
+        this.selectedEmployees.splice(index, 1);
+        console.log(this.selectedEmployees);
+        }
+        else{
+        this.selectedEmployees.push(obj);
+        console.log(this.selectedEmployees);
+        }
+    }
+
+    findIndexToUpdate(obj) { 
+            return obj === this;
+    }
+    
+
 }
